1c3d99cc00495f25a7bbd8c31004adf97141f582
Taylor Barinka <barinkataylor@gmail.com>
2022-11-13 16:03:49 -0600

    add X coordinates across top of DOM  game boards

--- src/userInterface/DOMloader.js
+++ src/userInterface/DOMloader.js
@@ -34,10 +34,37 @@ function singleBoardLoaderDOM(board) {
 };
 function twoBoardLoader(playerBoard, opponentBoard) {
     let container = document.createElement('div');
-    container.classList.add('boards-container')
-    container.appendChild(singleBoardLoaderDOM(playerBoard));
-    container.appendChild(singleBoardLoaderDOM(opponentBoard));
+    container.classList.add('boards-container');
+    let subcontainer = document.createElement('div');
+    subcontainer.classList.add('boards-subcontainer');
+    subcontainer.appendChild(singleBoardLoaderDOM(playerBoard));
+    subcontainer.appendChild(singleBoardLoaderDOM(opponentBoard));
+
+    container.appendChild(xCoordinateDoubler());
+    container.appendChild(subcontainer);
     document.body.appendChild(container)
+}
+function xCoordinateLoader() {
+    let arrayOfXCoordinates = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+    let container = document.createElement('div');
+    container.classList.add('xCoordinateContainer');
+    arrayOfXCoordinates.forEach((coordinate) => {
+        let square = document.createElement('div');
+        square.classList.add('upperSquare');
+        square.textContent = coordinate;
+        container.appendChild(square);
+    });
+    return container;
+}
+function xCoordinateDoubler() {
+    let container = document.createElement('div');
+    container.classList.add('xCoordinateContainerDouble');
+    container.appendChild(xCoordinateLoader());
+    container.appendChild(xCoordinateLoader());
+    return container;
+}
+function yCoordinateLoader() {
+
 }
 class gameBoardLoader {
     constructor(board, player, opponent) {
