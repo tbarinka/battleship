3b8c1ff218bb5cab5188bdb06b8a5df31d9db8cf
Taylor Barinka <barinkataylor@gmail.com>
2022-11-14 16:53:36 -0600

    add refresh button (no refresh functionality yet

--- src/userInterface/controller.js
+++ src/userInterface/controller.js
@@ -1,22 +1,36 @@
 import { gameBoardLoader } from './boardDOMloader.js';
 import { Gameboard } from '../gameAppLogic/gameboard.js';
 
+let playerBoard = new Gameboard();
+let aiBoard = new Gameboard();
+let player = "taylor"
+let ai = "computer"
+let boards = new gameBoardLoader(playerBoard, player, aiBoard, ai);
+const playerPopulateButton = document.createElement('button');
+playerPopulateButton.addEventListener('click', simplePopulate);
+playerPopulateButton.classList.add('populateButton');
+playerPopulateButton.textContent = "Populate Player";
+const aiPopulateButton = document.createElement('button');
+aiPopulateButton.addEventListener('click', simplePopulateAI);
+aiPopulateButton.classList.add('populateButton');
+aiPopulateButton.textContent = "Populate AI";
+const refreshButton = document.createElement('button');
+refreshButton.addEventListener('click', refresh);
+refreshButton.classList.add('populateButton');
+refreshButton.textContent = "Refresh [code not rdy"
+
 function generateHUD() {
     boards;
     document.body.appendChild(generateButtons())
 }
 function generateButtons() {
     let container = document.createElement('div');
+    container.classList.add('buttonContainer');
     container.appendChild(playerPopulateButton);
     container.appendChild(aiPopulateButton);
+    container.appendChild(refreshButton);
     return container;
 }
-
-let playerBoard = new Gameboard();
-let aiBoard = new Gameboard();
-let player = "taylor"
-let ai = "computer"
-let boards = new gameBoardLoader(playerBoard, player, aiBoard, ai);
 
 function simplePopulate() {
     boards.simplePopulate();
@@ -24,17 +38,9 @@ function simplePopulate() {
 function simplePopulateAI() {
     boards.simplePopulateAI();
 }
+function refresh() {
 
-const playerPopulateButton = document.createElement('button');
-playerPopulateButton.addEventListener('click', simplePopulate);
-playerPopulateButton.classList.add('populateButton');
-playerPopulateButton.textContent = "Populate Player";
-
-const aiPopulateButton = document.createElement('button');
-aiPopulateButton.addEventListener('click', simplePopulateAI);
-aiPopulateButton.classList.add('populateButton');
-aiPopulateButton.textContent = "Populate AI"
-
+}
 
 
 export { generateHUD }
