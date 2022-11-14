d96316e58edac5d6b2f3aa30717665af9ef79911
Taylor Barinka <barinkataylor@gmail.com>
2022-11-14 16:38:55 -0600

    add simplePopulateAI() method to gameBoardLoader

--- src/userInterface/controller.js
+++ src/userInterface/controller.js
@@ -1,25 +1,40 @@
 import { gameBoardLoader } from './boardDOMloader.js';
 import { Gameboard } from '../gameAppLogic/gameboard.js';
 
+function generateHUD() {
+    boards;
+    document.body.appendChild(generateButtons())
+}
+function generateButtons() {
+    let container = document.createElement('div');
+    container.appendChild(playerPopulateButton);
+    container.appendChild(aiPopulateButton);
+    return container;
+}
 
 let playerBoard = new Gameboard();
 let aiBoard = new Gameboard();
 let player = "taylor"
 let ai = "computer"
 let boards = new gameBoardLoader(playerBoard, player, aiBoard, ai);
+
 function simplePopulate() {
     boards.simplePopulate();
 }
-
-const button = document.createElement('button');
-button.addEventListener('click', simplePopulate);
-button.classList.add('populateButton');
-button.textContent = "Populate";
-
-function generateHUD() {
-    boards;
-    document.body.appendChild(button);
+function simplePopulateAI() {
+    boards.simplePopulateAI();
 }
 
+const playerPopulateButton = document.createElement('button');
+playerPopulateButton.addEventListener('click', simplePopulate);
+playerPopulateButton.classList.add('populateButton');
+playerPopulateButton.textContent = "Populate Player";
 
-export { button, boards, generateHUD }
+const aiPopulateButton = document.createElement('button');
+aiPopulateButton.addEventListener('click', simplePopulateAI);
+aiPopulateButton.classList.add('populateButton');
+aiPopulateButton.textContent = "Populate AI"
+
+
+
+export { generateHUD }
